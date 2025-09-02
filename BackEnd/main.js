90fbd12f3950c0bbe8c0288102271577959450ba
Edson Cássio Barcelos <59491303+edsonbarcellos02@users.noreply.github.com>
2025-09-02 09:46:57 -0300

    chore(config) Corrige erro de porta default e caminho da base

--- BackEnd/main.js
+++ BackEnd/main.js
@@ -1,5 +1,5 @@
 import app from './src/app.js';
 
-app.listen(process.env.PORT || 3000, ()=>{
-    console.log(`Server running`);
+app.listen(process.env.PORT || 8033, ()=>{
+    console.log(`Server running ${process.env.PORT}`);
 });
